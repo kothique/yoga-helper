aa382e07c395c7b590a350651eaa969724f45b48
Kothique Félixx <kothique@protonmail.ch>
2019-06-13 00:28:17 +0300

    Header

---
+++ src/renderer/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header id="header">
+    <Button @click.native="$router.push({ name: 'asanas' })">Asanas</Button>
+    <Button @click.native="$router.push({ name: 'cycles' })">Cycles</Button>
+    <Button @click.native="$router.push({ name: 'options' })">Options</Button>
+    <hr />
+  </header>
+</template>
+
+<script>
+  import Button from '@/components/Button'
+  
+  export default {
+    components: {
+      Button
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
